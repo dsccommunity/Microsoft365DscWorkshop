de5b758c494369568ae41cbdb7200f8142d64ee8
Raimund Andree <r.andree@live.com>
2024-02-25 21:34:02 +0100

    +Error handling

--- .build/CleanupAfterBuild.ps1
+++ .build/CleanupAfterBuild.ps1
@@ -1,7 +1,10 @@
 task ModuleCleanupBeforeBuild {
 
     #This task is subject to change depending on changing dependencies of the required modules
-    Write-Host "Cleaning up the required modules directory before the build process starts"
-    Remove-Item -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.15.0\ -Recurse -Force 
+    Write-Host 'Cleaning up the required modules directory before the build process starts'
+    if (Test-Path -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.15.0)
+    {
+        Remove-Item -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.15.0 -Recurse -Force
+    }
 
 }
