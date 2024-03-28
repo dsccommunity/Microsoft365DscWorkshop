d7fd7348225ba9139ec0e5fe38c8e748a594779f
Raimund Andree <r.andree@live.com>
2024-03-28 14:29:09 +0100

    Fixed and added back 'ModuleCleanupBeforeBuild'

--- .build/CleanupAfterBuild.ps1
+++ .build/CleanupAfterBuild.ps1
@@ -2,9 +2,9 @@ task ModuleCleanupBeforeBuild {
 
     #This task is subject to change depending on changing dependencies of the required modules
     Write-Host 'Cleaning up the required modules directory before the build process starts'
-    if (Test-Path -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.15.0)
+    if (Test-Path -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.16.0)
     {
-        Remove-Item -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.15.0 -Recurse -Force
+        Remove-Item -Path $RequiredModulesDirectory\Microsoft.Graph.Authentication\2.16.0 -Recurse -Force
     }
 
 }
